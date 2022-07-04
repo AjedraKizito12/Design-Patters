47331369f50a06cbbacb1ce40590cb202aca009c
Ajdra Kizito <ajdrakizito@gmail.com>
2022-07-04 18:46:03 +0300

    added data input and tracking

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import { UncontrolledOnboardingFlow } from "./components/UncontrolledOnboardingFlow";
 
-const StepOne = ({ goToNext }) => {
+const StepOne = ({ goToNext }) => (
   <>
     <h1>Step 1</h1>
-    <button onClick={goToNext}>Next</button>
-  </>;
-};
-const StepTwo = ({ goToNext }) => {
+    <button onClick={() => goToNext({ name: "Ajex Kizz" })}>Next</button>
+  </>
+);
+
+const StepTwo = ({ goToNext }) => (
   <>
     <h1>Step 2</h1>
-    <button onClick={goToNext}>Next</button>
-  </>;
-};
-const StepThree = ({ goToNext }) => {
+    <button onClick={() => goToNext({ age: 25 })}>Next</button>
+  </>
+);
+const StepThree = ({ goToNext }) => (
   <>
     <h1>Step 3</h1>
     <button onClick={goToNext}>Next</button>
-  </>;
-};
+  </>
+);
 function App() {
   return (
     <UncontrolledOnboardingFlow>
