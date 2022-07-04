a95925883807b0e38cc815399076289f050d13b3
Ajdra Kizito <ajdrakizito@gmail.com>
2022-07-04 18:46:27 +0300

    added dataloaading

--- src/components/UncontrolledOnboardingFlow.js
+++ src/components/UncontrolledOnboardingFlow.js
@@ -4,13 +4,28 @@ export const UncontrolledOnboardingFlow = ({ children, onFinish }) => {
   const [onboardingData, setOnboardingData] = useState({});
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const goToNext = () => {
-    setCurrentIndex(currentIndex + 1);
+  const goToNext = (stepData) => {
+    const nextIndex = currentIndex + 1;
+
+    const updatedData = {
+      ...onboardingData,
+      ...stepData,
+    };
+
+    if (nextIndex < children.length) {
+      setCurrentIndex(nextIndex);
+    } else {
+      onFinish(updatedData);
+    }
+
+    setOnboardingData(updatedData);
   };
+
   const currentChild = React.Children.toArray(children)[currentIndex];
 
   if (React.isValidElement(currentChild)) {
     return React.cloneElement(currentChild, { goToNext });
   }
-  return currentChild;
+
+  return { currentChild };
 };
